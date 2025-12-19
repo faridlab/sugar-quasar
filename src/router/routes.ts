7ca5678f17a1f8bda8b604638f7391a268d630ca
Farid Hidayat <farid@startapp.id>
2025-12-20 02:12:38 +0700

    feat: add simplified router with TypeScript

---
+++ src/router/routes.ts
@@ -0,0 +1,30 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import('pages/IndexPage.vue'),
+      },
+    ],
+  },
+
+  // Auth routes
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('pages/auth/LoginPage.vue'),
+  },
+
+  // Always leave this as last one
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
